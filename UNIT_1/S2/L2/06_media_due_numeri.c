cba9d635ceac0bfc14e748350321a9d71173442f
Niko82-ops <domenicovitale1982@libero.it>
2025-12-02 16:24:57 +0100

    Add files via upload

---
+++ UNIT_1/S2/L2/06_media_due_numeri.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main(){
+ 
+    int numero1 = 8;   
+    int numero2 = 12;
+    float media;
+ 
+    media = (numero1 + numero2) / 2.0;
+    printf("il primo numero è: %d\n", numero1);
+    printf("il secondo numero è: %d\n", numero2);
+    printf("La media dei numeri è: %.2f\n", media);
+return 0;
+}
