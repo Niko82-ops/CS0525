1631fa7f9d13b78865c6d4cfd07236d13e67d464
Niko82-ops <domenicovitale1982@libero.it>
2025-12-03 18:15:13 +0100

    Add files via upload

---
+++ UNIT_1/S2/L3/04_nome_band.py
@@ -0,0 +1,10 @@
+# Richiesta di input all' utente
+city = input("Inserisci il nome della tua città di origine: ")
+animal = input("Inserisci il nome del tuo animale domestico: ")
+
+
+# Generazione del nome della band
+nome_band = city + " " + animal
+
+# Output del risultato
+print("Il nome della tua band potrebbe essere:", nome_band)
